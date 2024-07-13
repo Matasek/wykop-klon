d7aeaf768106190aac3dffc15b7e90926a717362
Maciej <maciejmatasek@gmail.com>
2024-07-13 14:52:53 +0200

    Added files: day.css, night.css

--- slider.js
+++ slider.js
@@ -1,10 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
   const toggleSwitch = document.getElementById("toggleSwitch");
+  const themeStylesheet = document.getElementById("themeStylesheet");
+
   toggleSwitch.addEventListener("change", function () {
     if (this.checked) {
       console.log("Switched On");
+      themeStylesheet.setAttribute("href", "night.css");
     } else {
       console.log("Switched Off");
+      themeStylesheet.setAttribute("href", "day.css");
     }
   });
 });
