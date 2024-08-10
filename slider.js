2e60457315d55b56a4a7a9719356f5a592efcdbf
Maciej <maciejmatasek@gmail.com>
2024-08-10 22:13:13 +0200

    Set default mode to day and update slider text to match mode

--- slider.js
+++ slider.js
@@ -1,12 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   const toggleSwitch = document.getElementById("toggleSwitch");
   const themeStylesheet = document.getElementById("themeStylesheet");
+  const text = document.getElementById("night-mode-txt")
 
   toggleSwitch.addEventListener("change", function () {
     if (this.checked) {
       themeStylesheet.setAttribute("href", "night.css");
+      text.textContent = "Tryb nocny"
     } else {
       themeStylesheet.setAttribute("href", "day.css");
+      text.textContent = "Tryb dzienny"
     }
   });
 });
