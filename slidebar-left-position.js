1f8aaf08019e19975cb999b9bcc79bb674c383ca
Maciej Kryjom <maciejmatasek@gmail.com>
2024-08-16 20:17:37 +0200

    Update responsive.css for white-background with day/night style

--- slidebar-left-position.js
+++ slidebar-left-position.js
@@ -1,8 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
     const headerButton = document.getElementById("header-button");
     const slidebarLeft = document.getElementById("slidebar-left");
+    const whiteBackground = document.getElementById("white-background")
   
     headerButton.addEventListener("click", function () {
       slidebarLeft.classList.toggle("show");
+      if (themeStylesheet.getAttribute("href") === "night.css") {
+        whiteBackground.classList.toggle("show-night");
+    } else {
+      
+        whiteBackground.classList.toggle("show");
+
+    }
     });
   });
+
+
--- slider.js
+++ slider.js
@@ -2,14 +2,21 @@ document.addEventListener("DOMContentLoaded", () => {
   const toggleSwitch = document.getElementById("toggleSwitch");
   const themeStylesheet = document.getElementById("themeStylesheet");
   const text = document.getElementById("night-mode-txt")
+  const slidebarLeft = document.getElementById("slidebar-left");
+  const whiteBackground = document.getElementById("white-background")
 
   toggleSwitch.addEventListener("change", function () {
     if (this.checked) {
       themeStylesheet.setAttribute("href", "night.css");
       text.textContent = "Tryb nocny"
+      whiteBackground.classList.remove("show");
+      whiteBackground.classList.remove("show-night");
     } else {
       themeStylesheet.setAttribute("href", "day.css");
       text.textContent = "Tryb dzienny"
+      whiteBackground.classList.remove("show");
+      whiteBackground.classList.remove("show-night");
     }
+    slidebarLeft.classList.toggle("show");
   });
 });
