8ce85d9a492a77938da3ace73978677bb4c87463
Maciej Kryjom <maciejmatasek@gmail.com>
2024-08-17 21:45:44 +0200

    Added .slidebar-left-small to index

--- slidebar-left-position.js
+++ slidebar-left-position.js
@@ -1,18 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const headerButton = document.getElementById("header-button");
-    const slidebarLeft = document.getElementById("slidebar-left");
-    const whiteBackground = document.getElementById("white-background")
-  
-    headerButton.addEventListener("click", function () {
-      slidebarLeft.classList.toggle("show");
-      if (themeStylesheet.getAttribute("href") === "night.css") {
-        whiteBackground.classList.toggle("show-night");
+  const headerButton = document.getElementById("header-button");
+  const slidebarLeft = document.getElementById("slidebar-left");
+  const whiteBackground = document.getElementById("white-background");
+
+  headerButton.addEventListener("click", function () {
+    slidebarLeft.classList.toggle("show");
+    if (themeStylesheet.getAttribute("href") === "night.css") {
+      whiteBackground.classList.toggle("show-night");
     } else {
-    
-        whiteBackground.classList.toggle("show");
-
+      whiteBackground.classList.toggle("show");
     }
-    });
   });
-
-
+});
--- slider.js
+++ slider.js
@@ -1,19 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   const toggleSwitch = document.getElementById("toggleSwitch");
   const themeStylesheet = document.getElementById("themeStylesheet");
-  const text = document.getElementById("night-mode-txt")
+  const text = document.getElementById("night-mode-txt");
   const slidebarLeft = document.getElementById("slidebar-left");
-  const whiteBackground = document.getElementById("white-background")
+  const whiteBackground = document.getElementById("white-background");
 
   toggleSwitch.addEventListener("change", function () {
     if (this.checked) {
       themeStylesheet.setAttribute("href", "night.css");
-      text.textContent = "Tryb nocny"
+      text.textContent = "Tryb nocny";
       whiteBackground.classList.remove("show");
       whiteBackground.classList.remove("show-night");
     } else {
       themeStylesheet.setAttribute("href", "day.css");
-      text.textContent = "Tryb dzienny"
+      text.textContent = "Tryb dzienny";
       whiteBackground.classList.remove("show");
       whiteBackground.classList.remove("show-night");
     }
